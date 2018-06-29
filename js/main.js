84be67ba4fce382603b55d796ee3ee88d8eab09d
Harald Westphal <h.westphal@gmx.de>
2018-06-29 19:42:46 +0200

    add visualization

--- js/main.js
+++ js/main.js
@@ -1,90 +1,103 @@
-window['libopenmpt'] = {};
-libopenmpt.locateFile = function (filename) {
-    return "//cdn.rawgit.com/deskjet/chiptune2.js/master/" + filename;
-};
-libopenmpt.onRuntimeInitialized = function () {
-    var player;
+window.libopenmpt = {
+    locateFile: function (filename) {
+        return "//cdn.rawgit.com/deskjet/chiptune2.js/master/" + filename;
+    },
 
-    function init() {
-        if (player == undefined) {
-            player = new ChiptuneJsPlayer(new ChiptuneJsConfig(-1));
+    onRuntimeInitialized: function () {
+        var player, analyzer, bufferLength, dataArray, canvasCtx;
+
+        function init() {
+            if (player === undefined) {
+                player = new ChiptuneJsPlayer(new ChiptuneJsConfig(-1));
+                analyzer = player.context.createAnalyser();
+                analyzer.fftSize = 64;
+                bufferLength = analyzer.frequencyBinCount;
+                dataArray = new Uint8Array(bufferLength);
+                analyzer.connect(player.context.destination);
+                canvasCtx = document.getElementById('viz').getContext('2d');
+                visualize();
+            } else {
+                player.stop();
+                playPauseButton();
+            }
         }
-        else {
-            player.stop();
-            playPauseButton();
+
+        function visualize() {
+            var width = canvasCtx.canvas.width,
+                height = canvasCtx.canvas.height,
+                barWidth = (width / bufferLength) * 1.5,
+                barHeight,
+                x = 0;
+            requestAnimationFrame(visualize);
+            analyzer.getByteFrequencyData(dataArray);
+            canvasCtx.clearRect(0, 0, width, height);
+            for (var i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i] / 2;
+                canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ', 50, 50)';
+                canvasCtx.fillRect(x, height - barHeight / 2, barWidth, barHeight);
+                x += barWidth + 1;
+            }
         }
+
+        function setTitle(title) {
+            document.getElementById('title').innerHTML = title;
+        }
+
+        function afterLoad(path, buffer) {
+            document.querySelectorAll('#pitch,#tempo').forEach(e => e.value = 1);
+            player.play(buffer);
+            player.currentPlayingNode.disconnect();
+            player.currentPlayingNode.connect(analyzer);
+            pausePauseButton();
+        }
+
+        function loadURL(path, title) {
+            init();
+            player.load(path, afterLoad.bind(this, path));
+            setTitle(title);
+        }
+
+        function pauseButton() {
+            player.togglePause();
+            switchPauseButton();
+        }
+
+        function switchPauseButton() {
+            var button = document.getElementById('pause')
+            if (button) {
+                button.id = "play_tmp";
+            }
+            button = document.getElementById('play')
+            if (button) {
+                button.id = "pause";
+            }
+            button = document.getElementById('play_tmp')
+            if (button) {
+                button.id = "play";
+            }
+        }
+
+        function playPauseButton() {
+            var button = document.getElementById('pause')
+            if (button) {
+                button.id = "play";
+            }
+        }
+
+        function pausePauseButton() {
+            var button = document.getElementById('play')
+            if (button) {
+                button.id = "pause";
+            }
+        }
+
+        document.querySelectorAll('.song').forEach(function (e) {
+            e.addEventListener('click', function (evt) {
+                var target = evt.target;
+                loadURL(target.getAttribute("data-modurl"), evt.target.textContent);
+            }, false);
+        });
+
+        document.querySelector('#play').addEventListener('click', pauseButton, false);
     }
-
-    function setMetadata(filename) {
-        var metadata = player.metadata();
-        if (metadata['title'] != '') {
-            document.getElementById('title').innerHTML = metadata['title'];
-        }
-        else {
-            document.getElementById('title').innerHTML = filename;
-        }
-    }
-
-    function afterLoad(path, buffer) {
-        document.querySelectorAll('#pitch,#tempo').forEach(e => e.value = 1);
-        player.play(buffer);
-        setMetadata(path);
-        pausePauseButton();
-    }
-
-    function loadURL(path) {
-        init();
-        player.load(path, afterLoad.bind(this, path));
-    }
-
-    function pauseButton() {
-        player.togglePause();
-        switchPauseButton();
-    }
-
-    function switchPauseButton() {
-        var button = document.getElementById('pause')
-        if (button) {
-            button.id = "play_tmp";
-        }
-        button = document.getElementById('play')
-        if (button) {
-            button.id = "pause";
-        }
-        button = document.getElementById('play_tmp')
-        if (button) {
-            button.id = "play";
-        }
-    }
-
-    function playPauseButton() {
-        var button = document.getElementById('pause')
-        if (button) {
-            button.id = "play";
-        }
-    }
-
-    function pausePauseButton() {
-        var button = document.getElementById('play')
-        if (button) {
-            button.id = "pause";
-        }
-    }
-
-    document.querySelectorAll('.song').forEach(function (e) {
-        e.addEventListener('click', function (evt) {
-            modurl = evt.target.getAttribute("data-modurl");
-            loadURL(modurl);
-        }, false);
-    });
-
-    document.querySelector('#play').addEventListener('click', pauseButton, false);
-
-    document.querySelector('#pitch').addEventListener('input', function (e) {
-        player.module_ctl_set('play.pitch_factor', e.target.value.toString());
-    }, false);
-
-    document.querySelector('#tempo').addEventListener('input', function (e) {
-        player.module_ctl_set('play.tempo_factor', e.target.value.toString());
-    }, false);
 };
